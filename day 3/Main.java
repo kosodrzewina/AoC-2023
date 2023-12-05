ea200fc325605759ecd8c436df0313076ccd68f3
kosodrzewina <sienkiew@live.com>
2023-12-05 23:45:31 +0100

    day 3 part 1

---
+++ day 3/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        var part1 = new Part1("input.txt");
+
+        part1.run();
+
+        System.out.println(part1.getResult());
+    }
+}
